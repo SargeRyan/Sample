61a60a3e36dc9c5f0435f1bdf47f65c2dd1d92c2
Ryan Josua Sargento <ryanjoshua.sargeto@cvsu.edu.ph>
2024-02-12 23:23:40 +0800

    exerciseModule

--- myApp/Screens/ExercisePlan/component/exerciseList.js
+++ myApp/Screens/ExercisePlan/component/exerciseList.js
@@ -3,68 +3,78 @@ export const exerciseList = [
         "mealName": "Jogging",
         "mealImage": "https://drive.google.com/uc?id=1gVjFqT38I-YlQAX1qMu7g70mWDvlSnrZ",
         "caloriesBurn" : "225",
-        "duration" : "180"
-
+        "duration" : "60",
+        "notGoodFor": ["Stroke", "High Blood", "Asthma", "Diabetes", "Fatigue", "Arthritis"],
+        "exerciseVid": "https://drive.google.com/uc?id=1dCLACGwJjD8W4HewbcrwNf_r_wm4yJTn"
     },
 
      {
         "mealName": "Walking",
         "mealImage": "https://drive.google.com/uc?id=1KR9FudeMZO6i7t_jbzrN0E3c7jDzzqMx",
         "caloriesBurn" : "112",
-        "duration" : "180"
-
+        "exeDuration": [{"duration": "5", "type": "exercise"}, {"duration": "10", "type": "Rest"}, {"duration": "180", "type": "exercise"}, {"duration": "10", "Rest": "exercise"}, {"duration": "180", "type": "exercise"}, {"duration": "10", "type": "Rest"}],
+        "notGoodFor": ["Fatigue"],
+        "exerciseVid": "https://drive.google.com/uc?id=1dCLACGwJjD8W4HewbcrwNf_r_wm4yJTn"
     },
 
      {
         "mealName": "Standing March",
         "mealImage": "https://drive.google.com/uc?id=1f1s0vWtyuUFUaVCjCXzPkqULnvyMxO95",
         "caloriesBurn" : "84",
-        "duration" : "180"
-
+        "duration" : "180",
+        "notGoodFor": ["Fatigue"],
+        "exerciseVid": "https://drive.google.com/uc?id=1dCLACGwJjD8W4HewbcrwNf_r_wm4yJTn"
     },
 
       {
         "mealName": "Jumping Jack",
         "mealImage": "https://drive.google.com/uc?id=19reFIaZbhL80wTHP-Owui7MqQT3pKTw9",
         "caloriesBurn" : "153",
-        "duration" : "180"
-
+        "duration" : "180",
+        "notGoodFor": ["Fatigue", "Stroke", "Arthritis", "Asthma", "High Blood"]
     },
       {
         "mealName": "Light Dumbbell",
         "mealImage": "https://drive.google.com/uc?id=18JFpva9Q1HgMWxFL1d9fx3tqplzlLCs_",
         "caloriesBurn" : "84",
-        "duration" : "180"
+        "duration" : "180",
+        "notGoodFor": ["Fatigue"]
     },
      {
         "mealName": "Arm Rotation",
         "mealImage": "https://drive.google.com/uc?id=1G1SH7drenGCI6wBavG-Yaon1BhsEY1pI",
         "caloriesBurn" : "153",
-        "duration" : "180"
+        "duration" : "180",
+        "notGoodFor": ["g"]
     },
      {
         "mealName": "Knee Rotation",
         "mealImage": "https://drive.google.com/uc?id=1b_Vo0qepcvI0PmoCFMVD1u9jLY6sZxVx",
         "caloriesBurn" : "54",
-        "duration" : "180"
+        "duration" : "180",
+        "notGoodFor": ["g"]
     },
     {
         "mealName": "Head Rotation",
         "mealImage": "https://drive.google.com/uc?id=1z0XBwTdyc2ahDs1VUGlzBGUffun2wDLI",
         "caloriesBurn" : "18",
-        "duration" : "180"
+        "duration" : "180",
+        "notGoodFor": ["g"]
     },
     {
         "mealName": "Hip Rotation",
         "mealImage": "https://drive.google.com/uc?id=1QxlA8Ef6S4rFIg4vn3mhH-q3BfVIhzsk",
         "caloriesBurn" : " 67",
-        "duration" : "180"
+        "duration" : "180",
+        "notGoodFor": ["g"]
+
     },
      {
         "mealName": "Shoulder Roll",
         "mealImage": "https://drive.google.com/uc?id=1wT2Vfi6zI4gI8uf2CVd22hB8Ad8PkQhq",
         "caloriesBurn" : "27",
-        "duration" : "180"
+        "duration" : "180",
+        "notGoodFor": ["g"]
     },
 
 
